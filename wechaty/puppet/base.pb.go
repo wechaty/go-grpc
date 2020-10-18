8050b04c89530dce1c1e5599e10f6dc68c4eb754
Mike BO <mike@zixia.net>
2020-10-18 06:58:16 +0000

    Deploy Go Grpc Module v0.18.1

--- wechaty/puppet/base.pb.go
+++ wechaty/puppet/base.pb.go
@@ -7,7 +7,6 @@
 package puppet
 
 import (
-	proto "github.com/golang/protobuf/proto"
 	protoreflect "google.golang.org/protobuf/reflect/protoreflect"
 	protoimpl "google.golang.org/protobuf/runtime/protoimpl"
 	reflect "reflect"
@@ -21,9 +20,63 @@ const (
 	_ = protoimpl.EnforceVersion(protoimpl.MaxVersion - 20)
 )
 
-// This is a compile-time assertion that a sufficiently up-to-date version
-// of the legacy proto package is being used.
-const _ = proto.ProtoPackageIsVersion4
+type PayloadType int32
+
+const (
+	PayloadType_PAYLOAD_TYPE_UNKNOWN     PayloadType = 0
+	PayloadType_PAYLOAD_TYPE_MESSAGE     PayloadType = 1
+	PayloadType_PAYLOAD_TYPE_CONTACT     PayloadType = 2
+	PayloadType_PAYLOAD_TYPE_ROOM        PayloadType = 3
+	PayloadType_PAYLOAD_TYPE_ROOM_MEMBER PayloadType = 4
+	PayloadType_PAYLOAD_TYPE_FRIENDSHIP  PayloadType = 5
+)
+
+// Enum value maps for PayloadType.
+var (
+	PayloadType_name = map[int32]string{
+		0: "PAYLOAD_TYPE_UNKNOWN",
+		1: "PAYLOAD_TYPE_MESSAGE",
+		2: "PAYLOAD_TYPE_CONTACT",
+		3: "PAYLOAD_TYPE_ROOM",
+		4: "PAYLOAD_TYPE_ROOM_MEMBER",
+		5: "PAYLOAD_TYPE_FRIENDSHIP",
+	}
+	PayloadType_value = map[string]int32{
+		"PAYLOAD_TYPE_UNKNOWN":     0,
+		"PAYLOAD_TYPE_MESSAGE":     1,
+		"PAYLOAD_TYPE_CONTACT":     2,
+		"PAYLOAD_TYPE_ROOM":        3,
+		"PAYLOAD_TYPE_ROOM_MEMBER": 4,
+		"PAYLOAD_TYPE_FRIENDSHIP":  5,
+	}
+)
+
+func (x PayloadType) Enum() *PayloadType {
+	p := new(PayloadType)
+	*p = x
+	return p
+}
+
+func (x PayloadType) String() string {
+	return protoimpl.X.EnumStringOf(x.Descriptor(), protoreflect.EnumNumber(x))
+}
+
+func (PayloadType) Descriptor() protoreflect.EnumDescriptor {
+	return file_base_proto_enumTypes[0].Descriptor()
+}
+
+func (PayloadType) Type() protoreflect.EnumType {
+	return &file_base_proto_enumTypes[0]
+}
+
+func (x PayloadType) Number() protoreflect.EnumNumber {
+	return protoreflect.EnumNumber(x)
+}
+
+// Deprecated: Use PayloadType.Descriptor instead.
+func (PayloadType) EnumDescriptor() ([]byte, []int) {
+	return file_base_proto_rawDescGZIP(), []int{0}
+}
 
 type StartRequest struct {
 	state         protoimpl.MessageState
@@ -423,6 +476,99 @@ func (*DingResponse) Descriptor() ([]byte, []int) {
 	return file_base_proto_rawDescGZIP(), []int{9}
 }
 
+type DirtyPayloadRequest struct {
+	state         protoimpl.MessageState
+	sizeCache     protoimpl.SizeCache
+	unknownFields protoimpl.UnknownFields
+
+	Type PayloadType `protobuf:"varint,1,opt,name=type,proto3,enum=wechaty.puppet.PayloadType" json:"type,omitempty"`
+	Id   string      `protobuf:"bytes,2,opt,name=id,proto3" json:"id,omitempty"`
+}
+
+func (x *DirtyPayloadRequest) Reset() {
+	*x = DirtyPayloadRequest{}
+	if protoimpl.UnsafeEnabled {
+		mi := &file_base_proto_msgTypes[10]
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		ms.StoreMessageInfo(mi)
+	}
+}
+
+func (x *DirtyPayloadRequest) String() string {
+	return protoimpl.X.MessageStringOf(x)
+}
+
+func (*DirtyPayloadRequest) ProtoMessage() {}
+
+func (x *DirtyPayloadRequest) ProtoReflect() protoreflect.Message {
+	mi := &file_base_proto_msgTypes[10]
+	if protoimpl.UnsafeEnabled && x != nil {
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		if ms.LoadMessageInfo() == nil {
+			ms.StoreMessageInfo(mi)
+		}
+		return ms
+	}
+	return mi.MessageOf(x)
+}
+
+// Deprecated: Use DirtyPayloadRequest.ProtoReflect.Descriptor instead.
+func (*DirtyPayloadRequest) Descriptor() ([]byte, []int) {
+	return file_base_proto_rawDescGZIP(), []int{10}
+}
+
+func (x *DirtyPayloadRequest) GetType() PayloadType {
+	if x != nil {
+		return x.Type
+	}
+	return PayloadType_PAYLOAD_TYPE_UNKNOWN
+}
+
+func (x *DirtyPayloadRequest) GetId() string {
+	if x != nil {
+		return x.Id
+	}
+	return ""
+}
+
+type DirtyPayloadResponse struct {
+	state         protoimpl.MessageState
+	sizeCache     protoimpl.SizeCache
+	unknownFields protoimpl.UnknownFields
+}
+
+func (x *DirtyPayloadResponse) Reset() {
+	*x = DirtyPayloadResponse{}
+	if protoimpl.UnsafeEnabled {
+		mi := &file_base_proto_msgTypes[11]
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		ms.StoreMessageInfo(mi)
+	}
+}
+
+func (x *DirtyPayloadResponse) String() string {
+	return protoimpl.X.MessageStringOf(x)
+}
+
+func (*DirtyPayloadResponse) ProtoMessage() {}
+
+func (x *DirtyPayloadResponse) ProtoReflect() protoreflect.Message {
+	mi := &file_base_proto_msgTypes[11]
+	if protoimpl.UnsafeEnabled && x != nil {
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		if ms.LoadMessageInfo() == nil {
+			ms.StoreMessageInfo(mi)
+		}
+		return ms
+	}
+	return mi.MessageOf(x)
+}
+
+// Deprecated: Use DirtyPayloadResponse.ProtoReflect.Descriptor instead.
+func (*DirtyPayloadResponse) Descriptor() ([]byte, []int) {
+	return file_base_proto_rawDescGZIP(), []int{11}
+}
+
 var File_base_proto protoreflect.FileDescriptor
 
 var file_base_proto_rawDesc = []byte{
@@ -441,12 +587,32 @@ var file_base_proto_rawDesc = []byte{
 	0x0a, 0x0b, 0x44, 0x69, 0x6e, 0x67, 0x52, 0x65, 0x71, 0x75, 0x65, 0x73, 0x74, 0x12, 0x12, 0x0a,
 	0x04, 0x64, 0x61, 0x74, 0x61, 0x18, 0x01, 0x20, 0x01, 0x28, 0x09, 0x52, 0x04, 0x64, 0x61, 0x74,
 	0x61, 0x22, 0x0e, 0x0a, 0x0c, 0x44, 0x69, 0x6e, 0x67, 0x52, 0x65, 0x73, 0x70, 0x6f, 0x6e, 0x73,
-	0x65, 0x42, 0x4a, 0x0a, 0x1d, 0x69, 0x6f, 0x2e, 0x67, 0x69, 0x74, 0x68, 0x75, 0x62, 0x2e, 0x77,
+	0x65, 0x22, 0x56, 0x0a, 0x13, 0x44, 0x69, 0x72, 0x74, 0x79, 0x50, 0x61, 0x79, 0x6c, 0x6f, 0x61,
+	0x64, 0x52, 0x65, 0x71, 0x75, 0x65, 0x73, 0x74, 0x12, 0x2f, 0x0a, 0x04, 0x74, 0x79, 0x70, 0x65,
+	0x18, 0x01, 0x20, 0x01, 0x28, 0x0e, 0x32, 0x1b, 0x2e, 0x77, 0x65, 0x63, 0x68, 0x61, 0x74, 0x79,
+	0x2e, 0x70, 0x75, 0x70, 0x70, 0x65, 0x74, 0x2e, 0x50, 0x61, 0x79, 0x6c, 0x6f, 0x61, 0x64, 0x54,
+	0x79, 0x70, 0x65, 0x52, 0x04, 0x74, 0x79, 0x70, 0x65, 0x12, 0x0e, 0x0a, 0x02, 0x69, 0x64, 0x18,
+	0x02, 0x20, 0x01, 0x28, 0x09, 0x52, 0x02, 0x69, 0x64, 0x22, 0x16, 0x0a, 0x14, 0x44, 0x69, 0x72,
+	0x74, 0x79, 0x50, 0x61, 0x79, 0x6c, 0x6f, 0x61, 0x64, 0x52, 0x65, 0x73, 0x70, 0x6f, 0x6e, 0x73,
+	0x65, 0x2a, 0xad, 0x01, 0x0a, 0x0b, 0x50, 0x61, 0x79, 0x6c, 0x6f, 0x61, 0x64, 0x54, 0x79, 0x70,
+	0x65, 0x12, 0x18, 0x0a, 0x14, 0x50, 0x41, 0x59, 0x4c, 0x4f, 0x41, 0x44, 0x5f, 0x54, 0x59, 0x50,
+	0x45, 0x5f, 0x55, 0x4e, 0x4b, 0x4e, 0x4f, 0x57, 0x4e, 0x10, 0x00, 0x12, 0x18, 0x0a, 0x14, 0x50,
+	0x41, 0x59, 0x4c, 0x4f, 0x41, 0x44, 0x5f, 0x54, 0x59, 0x50, 0x45, 0x5f, 0x4d, 0x45, 0x53, 0x53,
+	0x41, 0x47, 0x45, 0x10, 0x01, 0x12, 0x18, 0x0a, 0x14, 0x50, 0x41, 0x59, 0x4c, 0x4f, 0x41, 0x44,
+	0x5f, 0x54, 0x59, 0x50, 0x45, 0x5f, 0x43, 0x4f, 0x4e, 0x54, 0x41, 0x43, 0x54, 0x10, 0x02, 0x12,
+	0x15, 0x0a, 0x11, 0x50, 0x41, 0x59, 0x4c, 0x4f, 0x41, 0x44, 0x5f, 0x54, 0x59, 0x50, 0x45, 0x5f,
+	0x52, 0x4f, 0x4f, 0x4d, 0x10, 0x03, 0x12, 0x1c, 0x0a, 0x18, 0x50, 0x41, 0x59, 0x4c, 0x4f, 0x41,
+	0x44, 0x5f, 0x54, 0x59, 0x50, 0x45, 0x5f, 0x52, 0x4f, 0x4f, 0x4d, 0x5f, 0x4d, 0x45, 0x4d, 0x42,
+	0x45, 0x52, 0x10, 0x04, 0x12, 0x1b, 0x0a, 0x17, 0x50, 0x41, 0x59, 0x4c, 0x4f, 0x41, 0x44, 0x5f,
+	0x54, 0x59, 0x50, 0x45, 0x5f, 0x46, 0x52, 0x49, 0x45, 0x4e, 0x44, 0x53, 0x48, 0x49, 0x50, 0x10,
+	0x05, 0x42, 0x67, 0x0a, 0x1d, 0x69, 0x6f, 0x2e, 0x67, 0x69, 0x74, 0x68, 0x75, 0x62, 0x2e, 0x77,
 	0x65, 0x63, 0x68, 0x61, 0x74, 0x79, 0x2e, 0x67, 0x72, 0x70, 0x63, 0x2e, 0x70, 0x75, 0x70, 0x70,
 	0x65, 0x74, 0x5a, 0x29, 0x67, 0x69, 0x74, 0x68, 0x75, 0x62, 0x2e, 0x63, 0x6f, 0x6d, 0x2f, 0x77,
 	0x65, 0x63, 0x68, 0x61, 0x74, 0x79, 0x2f, 0x67, 0x6f, 0x2d, 0x67, 0x72, 0x70, 0x63, 0x2f, 0x77,
-	0x65, 0x63, 0x68, 0x61, 0x74, 0x79, 0x2f, 0x70, 0x75, 0x70, 0x70, 0x65, 0x74, 0x62, 0x06, 0x70,
-	0x72, 0x6f, 0x74, 0x6f, 0x33,
+	0x65, 0x63, 0x68, 0x61, 0x74, 0x79, 0x2f, 0x70, 0x75, 0x70, 0x70, 0x65, 0x74, 0xaa, 0x02, 0x1a,
+	0x67, 0x69, 0x74, 0x68, 0x75, 0x62, 0x2e, 0x77, 0x65, 0x63, 0x68, 0x61, 0x74, 0x79, 0x2e, 0x67,
+	0x72, 0x70, 0x63, 0x2e, 0x70, 0x75, 0x70, 0x70, 0x65, 0x74, 0x62, 0x06, 0x70, 0x72, 0x6f, 0x74,
+	0x6f, 0x33,
 }
 
 var (
@@ -461,25 +627,30 @@ func file_base_proto_rawDescGZIP() []byte {
 	return file_base_proto_rawDescData
 }
 
-var file_base_proto_msgTypes = make([]protoimpl.MessageInfo, 10)
+var file_base_proto_enumTypes = make([]protoimpl.EnumInfo, 1)
+var file_base_proto_msgTypes = make([]protoimpl.MessageInfo, 12)
 var file_base_proto_goTypes = []interface{}{
-	(*StartRequest)(nil),    // 0: wechaty.puppet.StartRequest
-	(*StartResponse)(nil),   // 1: wechaty.puppet.StartResponse
-	(*StopRequest)(nil),     // 2: wechaty.puppet.StopRequest
-	(*StopResponse)(nil),    // 3: wechaty.puppet.StopResponse
-	(*VersionRequest)(nil),  // 4: wechaty.puppet.VersionRequest
-	(*VersionResponse)(nil), // 5: wechaty.puppet.VersionResponse
-	(*LogoutRequest)(nil),   // 6: wechaty.puppet.LogoutRequest
-	(*LogoutResponse)(nil),  // 7: wechaty.puppet.LogoutResponse
-	(*DingRequest)(nil),     // 8: wechaty.puppet.DingRequest
-	(*DingResponse)(nil),    // 9: wechaty.puppet.DingResponse
+	(PayloadType)(0),             // 0: wechaty.puppet.PayloadType
+	(*StartRequest)(nil),         // 1: wechaty.puppet.StartRequest
+	(*StartResponse)(nil),        // 2: wechaty.puppet.StartResponse
+	(*StopRequest)(nil),          // 3: wechaty.puppet.StopRequest
+	(*StopResponse)(nil),         // 4: wechaty.puppet.StopResponse
+	(*VersionRequest)(nil),       // 5: wechaty.puppet.VersionRequest
+	(*VersionResponse)(nil),      // 6: wechaty.puppet.VersionResponse
+	(*LogoutRequest)(nil),        // 7: wechaty.puppet.LogoutRequest
+	(*LogoutResponse)(nil),       // 8: wechaty.puppet.LogoutResponse
+	(*DingRequest)(nil),          // 9: wechaty.puppet.DingRequest
+	(*DingResponse)(nil),         // 10: wechaty.puppet.DingResponse
+	(*DirtyPayloadRequest)(nil),  // 11: wechaty.puppet.DirtyPayloadRequest
+	(*DirtyPayloadResponse)(nil), // 12: wechaty.puppet.DirtyPayloadResponse
 }
 var file_base_proto_depIdxs = []int32{
-	0, // [0:0] is the sub-list for method output_type
-	0, // [0:0] is the sub-list for method input_type
-	0, // [0:0] is the sub-list for extension type_name
-	0, // [0:0] is the sub-list for extension extendee
-	0, // [0:0] is the sub-list for field type_name
+	0, // 0: wechaty.puppet.DirtyPayloadRequest.type:type_name -> wechaty.puppet.PayloadType
+	1, // [1:1] is the sub-list for method output_type
+	1, // [1:1] is the sub-list for method input_type
+	1, // [1:1] is the sub-list for extension type_name
+	1, // [1:1] is the sub-list for extension extendee
+	0, // [0:1] is the sub-list for field type_name
 }
 
 func init() { file_base_proto_init() }
@@ -608,19 +779,44 @@ func file_base_proto_init() {
 				return nil
 			}
 		}
+		file_base_proto_msgTypes[10].Exporter = func(v interface{}, i int) interface{} {
+			switch v := v.(*DirtyPayloadRequest); i {
+			case 0:
+				return &v.state
+			case 1:
+				return &v.sizeCache
+			case 2:
+				return &v.unknownFields
+			default:
+				return nil
+			}
+		}
+		file_base_proto_msgTypes[11].Exporter = func(v interface{}, i int) interface{} {
+			switch v := v.(*DirtyPayloadResponse); i {
+			case 0:
+				return &v.state
+			case 1:
+				return &v.sizeCache
+			case 2:
+				return &v.unknownFields
+			default:
+				return nil
+			}
+		}
 	}
 	type x struct{}
 	out := protoimpl.TypeBuilder{
 		File: protoimpl.DescBuilder{
 			GoPackagePath: reflect.TypeOf(x{}).PkgPath(),
 			RawDescriptor: file_base_proto_rawDesc,
-			NumEnums:      0,
-			NumMessages:   10,
+			NumEnums:      1,
+			NumMessages:   12,
 			NumExtensions: 0,
 			NumServices:   0,
 		},
 		GoTypes:           file_base_proto_goTypes,
 		DependencyIndexes: file_base_proto_depIdxs,
+		EnumInfos:         file_base_proto_enumTypes,
 		MessageInfos:      file_base_proto_msgTypes,
 	}.Build()
 	File_base_proto = out.File
